6d9cced339bf77c8a8f2677c7506227fdedf3ed7
Offirmo <offirmo.net@gmail.com>
2024-01-11 09:08:06 +1100

    +++

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # hello-world--ai--2024
+
+goals
+[] go through https://ai-guide.future.mozilla.org/
+- https://huggingface.co/support
+[] go through https://app.datacamp.com/workspace/w/2da1f6ee-4d26-474f-b8f1-1ed72ed1596d/edit
+[] agents?? https://www.artifact.io/iap
+[] how does this work? https://github.com/ykhli/AI-tamago/blob/main/src/app/utils/interaction.ts
+[] https://9elements.com/blog/using-symbolic-logic-for-mitigating-nondeterministic-behavior-and-hallucinations-of-llms/
+[] fine tuning https://asmirnov.xyz/doppelganger
+[] ML system design https://www.evidentlyai.com/ml-system-design
+[] tool https://erichartford.com/running-dolphin-locally-with-ollama
+[] https://blog.gregbrockman.com/how-i-became-a-machine-learning-practitioner
+[] https://agentydragon.com/posts/2023-01-11-how-i-got-to-openai.html
+[] https://github.blog/2023-10-30-the-architecture-of-todays-llm-applications/
+
+
+```bash
+source ./python-virtualenv/bin/activate
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip         ## pip upgrade itself
+python3 -m pip install --upgrade setuptools  ## update setuptools
+
+...
+
+deactivate
+```
+
+```bash
+## https://huggingface.co/welcome
+pip install huggingface_hub
+
+
+
+```bash
+virtualenv python-venv
+```
+
+
+
+
+See also
+* 2023 LLM course https://github.com/mlabonne/llm-course
+* https://zapier.com/blog/best-ai-courses/
---
+++ src/introduction.py
@@ -0,0 +1,13 @@
+import textwrap
+
+content = """Mozilla's "Trustworthy AI" Thinking Points:
+
+PRIVACY: How is data collected, stored, and shared? Our personal data powers everything from traffic maps to targeted advertising. Trustworthy AI should enable people to decide how their data is used and what decisions are made with it.
+
+FAIRNESS: We’ve seen time and again how bias shows up in computational models, data, and frameworks behind automated decision making. The values and goals of a system should be power aware and seek to minimize harm. Further, AI systems that depend on human workers should protect people from exploitation and overwork.
+
+TRUST: People should have agency and control over their data and algorithmic outputs, especially considering the high stakes for individuals and societies. For instance, when online recommendation systems push people towards extreme, misleading content, potentially misinforming or radicalizing them.
+
+SAFETY: AI systems can carry high risk for exploitation by bad actors. Developers need to implement strong measures to protect our data and personal security. Further, excessive energy consumption and extraction of natural resources for computing and machine learning accelerates the climate crisis.
+
+TRANSPARENCY: Automated decisions can have huge personal impacts, yet the reasons for decisions are often opaque. We need to mandate transparency so that we can fully understand these systems and their potential for harm."""
